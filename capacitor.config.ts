d8f6b8507337ff22beae64e459e24ba25219e9ad
wario41 <wario81@outlook.com>
2025-08-01 10:08:41 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Solid sniffer',
+  webDir: 'www'
+};
+
+export default config;
